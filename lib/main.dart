91db0747e18456fab949eb2d48b406bed1250674
sagar salgar <s.s.sagarsalgar@gmail.com>
2025-03-31 19:28:43 +0530

    add the provider class in the runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
+import 'package:agri_chem/authentication/auth_gate.dart';
 import 'package:flutter/material.dart';
-import 'package:agri_chem/screens/home_screen.dart'; //home screen
 import 'package:flutter_dotenv/flutter_dotenv.dart'; //dotenv
 import 'package:agri_chem/themes/app_theme.dart'; //theme import
 
+import 'package:provider/provider.dart';
+
+//firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'package:agri_chem/firebase_options.dart';
+
 Future main() async {
-  await dotenv.load(fileName: ".env");
-  runApp(MyApp());
+  await dotenv.load(fileName: ".env"); //dotenv
+  WidgetsFlutterBinding.ensureInitialized(); //firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    /// Providers are above [MyApp] instead of inside it, so that tests
+    /// can use [MyApp] while mocking the providers
+    MultiProvider(providers: [], child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +29,8 @@ class MyApp extends StatelessWidget {
       title: "Agri Chem", //Application Name
       theme: AppTheme.lightTheme, //My theme
       darkTheme: AppTheme.darkTheme,
-      home: HomeScreen(),
+      home:
+          AuthGate(), //The app starts with the user being getting authenticating.
     );
   }
 }
