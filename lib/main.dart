a592970872d61ba4fda76bf5878c39cbf96d725a
sagar salgar <s.s.sagarsalgar@gmail.com>
2025-03-31 19:47:09 +0530

    add ios app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:agri_chem/authentication/auth_gate.dart';
-import 'package:agri_chem/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart'; //dotenv
 import 'package:agri_chem/themes/app_theme.dart'; //theme import
-
+import 'package:flutter/foundation.dart'; // for example of provider
 import 'package:provider/provider.dart';
 
 //firebase
@@ -17,8 +16,29 @@ Future main() async {
   runApp(
     /// Providers are above [MyApp] instead of inside it, so that tests
     /// can use [MyApp] while mocking the providers
-    MultiProvider(providers: [], child: const MyApp()),
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => Counter())],
+      child: const MyApp(),
+    ),
   );
+}
+
+class Counter with ChangeNotifier, DiagnosticableTreeMixin {
+  int _count = 0;
+
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
+
+  /// Makes `Counter` readable inside the devtools by listing all of its properties
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(IntProperty('count', count));
+  }
 }
 
 class MyApp extends StatelessWidget {
